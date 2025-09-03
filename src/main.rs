6a93d0e9cd697aefa2bcc74a9d3303ad8b49b038
bellobambo <bellobambo21@gmail.com>
2025-09-03 11:25:25 +0100

    array

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 fn main() {
-    let _unsigned: u8 = 10;
+    // let _unsigned: u8 = 10;
 
-    let _signed: i8 = -10;
+    // let _signed: i8 = -10;
 
-    let _float: f32 = -1.5;
+    // let _float: f32 = -1.5;
 
-    println!("unsign: {} sign: {} float: {}", _unsigned, _signed, _float);
+    // println!("unsign: {} sign: {} float: {}", _unsigned, _signed, _float);
 
-    let is_true: bool = true;
-    println!("is_true: {}", is_true);
+    // let is_true: bool = true;
+    // println!("is_true: {}", is_true);
+
+    let arr: [u8; 3] = [1, 2, 3];
+    let other_arr: [u8; 5] = [100, 5, 3, 6, 92];
+
+    println!("index: {}, length: {}", arr[0], other_arr.len());
+
+    println!("{:?}", other_arr);
 }
