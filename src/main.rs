3d4094323d50bcb0d1873bda33037b03718e00c1
bellobambo <bellobambo21@gmail.com>
2025-09-03 08:37:34 +0100

    rust init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let _unsigned: u8 = 10;
+
+    let _signed: i8 = -10;
+
+    let _float: f32 = -1.5;
+
+    println!("unsign: {} sign: {} float: {}", _unsigned, _signed, _float);
+
+    let is_true: bool = true;
+    println!("is_true: {}", is_true);
+}
